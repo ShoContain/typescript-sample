6ab11d61794d2ff7848d0d2733c127a96393ab28
田中 翔太 <perblue.666@gmail.com>
2021-12-20 14:47:18 +0900

    eslintとts-node導入

--- sample.js
+++ sample.js
@@ -1,4 +1,15 @@
 "use strict";
 var a = false;
 console.log(a);
+function func(a) {
+    return Number(a);
+}
+var number = func('4');
+console.log(number);
+var hoge = { a: 10, b: 3 };
+var first = hoge.a;
+var c = {
+    b: 12
+};
+console.log(c);
 //# sourceMappingURL=sample.js.map
--- sample.ts
+++ sample.ts
@@ -1,3 +1,32 @@
-let a: boolean = false;
+let a: boolean = false
 
-console.log(a)
+function func(a: string): number {
+    return Number(a)
+}
+
+const number: number = func('4')
+console.log(number)
+
+//Object
+interface Hoge {
+    a: Number
+    b: Number
+}
+
+let hoge: Hoge = { a: 10, b: 3 }
+
+let first: Number = hoge.a
+
+let c: { b: number } = {
+    b: 12,
+}
+
+let x: {
+    y: number
+    z?: string
+    [key: number]: boolean
+}
+
+// x = { b: 1 } //OK
+// x = { b: 1, c: '1' } // OK
+// x = { b: 1, c: '1', delete_flag: true } // OK
